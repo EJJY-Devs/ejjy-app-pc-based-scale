b5a6346de27261ff177c02bf957ddb35eee39365
Andrew Oplas <andrewmoplas@gmail.com>
2021-07-29 13:53:16 +0800

    Remove redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { APP_TITLE } from './global/constants';
 import Login from './screens/Login/Login';
 import Main from './screens/Main/Main';
@@ -13,8 +13,6 @@ const App = () => (
 		<Switch>
 			<Route path="/login" exact component={Login} />
 			<Route path="/main" exact component={Main} />
-
-			<Redirect from="/" to="/login" />
 		</Switch>
 	</>
 );
